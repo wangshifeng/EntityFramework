3335d24ac6f03d94c67b0ac06f11ce5869d44872
Andrew Peters <anpete@microsoft.com>
2017-05-15 15:34:16 -0700

    Query: Testing out a fix for #8393:
    - Fixes a race in async ShapedQuery execution.
    - Optimizes ShapedQuery by pushing down shaper to inner enumerable.
    - Removes IValueBufferCursor.Current - no longer used.

---
+++ src/EFCore.Relational/Query/ExpressionVisitors/Internal/NullShaper.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using Remotion.Linq.Clauses;
+
+namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
+{
+    public sealed class NullShaper : IShaper<ValueBuffer>
+    {
+        public static readonly NullShaper Instance = new NullShaper();
+
+        private NullShaper()
+        {
+        }
+
+        public ValueBuffer Shape(QueryContext queryContext, ValueBuffer valueBuffer) => valueBuffer;
+
+        public bool IsShaperForQuerySource(IQuerySource querySource) => false;
+
+        public void SaveAccessorExpression(QuerySourceMapping querySourceMapping)
+        {
+        }
+
+        public Expression GetAccessorExpression(IQuerySource querySource) => null;
+    }
+}
